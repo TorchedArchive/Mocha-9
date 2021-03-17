69bffc88b65d6edecbca90c64569da8dccac06a0
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-03-17 02:20:59 -0400

    update and some cleaning

--- main.go
+++ main.go
@@ -7,12 +7,27 @@ import (
 	"github.com/akamensky/argparse"
 )
 
+var font map[string]bitmap.Glyph
+
 func main() {
 	parser := argparse.NewParser("mocha-9", "A sweet warm fantasy brew.")
 
 	err := parser.Parse(os.Args)
 	if err != nil {
 		fmt.Print(parser.Usage(err))
+	}
+	
+	// Load bitmap font
+	// TODO: Use a different font
+	bm := bitmap.New()
+	font = bm.Load("m5x7.png")
+
+	// Load palette for
+	// TODO: Monochrome palette, like gameboy screen (green and green-black)
+	palettelib := palettenom.New()
+	colors, err := palettelib.Load("coffeebrew.png")
+	if err != nil {
+		panic(err)
 	}
 
 	Run()
--- mocha.go
+++ mocha.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"image/color"
+
 	"github.com/veandco/go-sdl2/sdl"
+	"github.com/PinwheelSystem/bitmap"
 )
 
+var vram []byte
+
 func Run() {
+	// Initialize VRAM
+	vram = make([]byte, 81 * 81 * 4)
+
+	// Initialize SDL
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
 	defer sdl.Quit()
 
+	// Setup our window
 	window, err := sdl.CreateWindow("Mocha-9", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 81 * 4, 81 * 4, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
 	defer window.Destroy()
 
+	// Setup renderer
 	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
 		panic(err)
 	}
 	defer renderer.Destroy()
 
+
+	// Setup SDL texture that will be used as our screen
 	screen, err := renderer.CreateTexture(sdl.PIXELFORMAT_ARGB8888, sdl.TEXTUREACCESS_STREAMING, 81, 81)
 	if err != nil {
 		panic(err)
@@ -31,7 +44,10 @@ func Run() {
 	running := true
 	renderer.SetDrawColor(0, 0, 0, sdl.ALPHA_OPAQUE)
 
+	// Actually Boot up Mocha by starting up Lua side
 	InitLua()
+
+	// SDl screen loop
 	for running {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch event.(type) {
@@ -41,5 +57,23 @@ func Run() {
 				break
 			}
 		}
+
+		// Update screen with VRAM content
+		screen.Update(nil, vram, 81 * 4)
+		// Copy to renderer
+		renderer.Copy(screen, nil, nil)
+
+		// Show updated screen
+		renderer.Present()
 	}
+}
+
+func setpixel(x, y int, c color.Color) {
+	offset := ( 81 * 4 * y ) + x * 4;
+	r, g, b, _ := c.RGBA()
+	vram[offset + 0] = byte(b)
+	vram[offset + 1] = byte(g)
+	vram[offset + 2] = byte(r)
+	vram[offset + 3] = sdl.ALPHA_OPAQUE;
+
 }
