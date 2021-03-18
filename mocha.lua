be1d37f266f4f11aac90fee6be4c9ad5d0e6b28a
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-03-17 22:05:14 -0400

    feat: line function

--- mocha.lua
+++ mocha.lua
@@ -26,6 +26,21 @@ setfunc("rect", function (x, y, w, h, color)
 	end
 end)
 
+setfunc("line", function (x1, y1, x2, y2, color) 
+	local x = x2 - x1
+	local y = y2 - y1
+	local length = math.sqrt((x * x) + (y * y))
+	local addx = x / length
+	local addy = y / length
+	local x = x1
+	local y = y1
+
+	for i = length, 0, -1 do
+		plot(x, y, color)
+		x = x + addx
+		y = y + addy
+	end
+end)
 
 dofile('program.lua')
 
